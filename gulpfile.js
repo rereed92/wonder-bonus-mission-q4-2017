2255b4858477075da87ba71dcb752d66811cf753
Rebecca Reed <rebeccareed212@hotmail.com>
2017-12-10 18:53:44 +0000

    Complete basic setup of gulp project

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const useref = require('gulp-useref');
+const uglify = require('gulp-uglify');
+const gulpIf = require('gulp-if');
+const cssnano = require('gulp-cssnano');
+const del = require('del');
+const runSequence = require('run-sequence');
 
 gulp.task('hello', function() {
     console.log('Hello Becky');
 });
+
+gulp.task('clean:dist', function() {
+    return del.sync('dist');
+})
 
 gulp.task('browserSync', function() {
     browserSync.init({
@@ -14,7 +24,7 @@ gulp.task('browserSync', function() {
     })
 });
 
-gulp.task('sass', function(){
+gulp.task('sass', function() {
     return gulp.src('src/scss/**/*.scss')
         .pipe(sass())
         .pipe(gulp.dest('src/css'))
@@ -23,8 +33,29 @@ gulp.task('sass', function(){
         }))
 });
 
-gulp.task('watch', ['browserSync', 'sass'], function(){
+gulp.task('useref', function() {
+    return gulp.src('src/*.html')
+        .pipe(useref())
+        .pipe(gulpIf('*.js', uglify()))
+        .pipe(gulpIf('*.css', cssnano()))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('watch', ['browserSync', 'sass'], function() {
     gulp.watch('src/scss/**/*.scss', ['sass']); 
     gulp.watch('src/*.html', browserSync.reload); 
     gulp.watch('src/js/**/*.js', browserSync.reload); 
+});
+
+gulp.task('build', function (callback) {
+    runSequence('clean:dist', 
+        ['sass', 'useref'],
+        callback
+    )
+});
+
+gulp.task('default', function (callback) {
+    runSequence(['sass', 'browserSync', 'watch'],
+        callback
+    )
 });
