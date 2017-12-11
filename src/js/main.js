40f14fc66e10333e043573812444118cf97b24e5
Rebecca Reed <rebeccareed@Rebeccas-MacBook-Pro.local>
2017-12-11 13:34:00 +0000

    Add in animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,46 @@
 (function($) {
 
-    // var panels = $('.box__panel').hide();
+    var boxes = $('.box')
+	var offset = .9;
 
-    // $('.box__content').click(function () {
-    //     $(this).next('.box__panel').toggleClass('box__panel--show').slideToggle();
-    // });
+	hideBoxes(boxes, offset);
+
+	$(window).scroll(function()
+	{
+		if (window.requestAnimationFrame)
+		{
+			window.requestAnimationFrame(function() { 
+				showBoxes(boxes, offset); 
+			});
+		}
+		else 
+		{
+			setTimeout(function() { 
+				showBoxes(boxes, offset); 
+			}, 100)
+		}
+	});
 
 })(jQuery);
+
+
+function hideBoxes(boxes, offset) 
+{
+	boxes.each(function() 
+	{
+		if ($(this).offset().top > $(window).scrollTop()+$(window).height() * offset) {
+			$(this).find('.box__content').addClass('box__content--hidden');
+		}
+	});
+}
+
+
+function showBoxes(boxes, offset) 
+{
+	boxes.each(function() 
+	{
+		if ($(this).offset().top <= $(window).scrollTop()+$(window).height()*offset && $(this).find('.box__content').hasClass('box__content--hidden')) { 
+			$(this).find('.box__content').removeClass('box__content--hidden').addClass('box__content--bounce');
+		}
+	});
+}
